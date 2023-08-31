b00c62d817c55299cdcc5e529100a6b11917936b
Nikolay <jejel.n.o@mail.ru>
2023-09-01 00:40:46 +0300

    task 231 last

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,80 @@
+package web.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotEmpty(message = "Add Name")
+    @Size(min = 3, max = 20, message = "3 < Name > 20")
+    @Column
+    private String name;
+
+    @Min(value = 14, message = "Age >= 14")
+    @Column
+    private byte age;
+
+    @NotEmpty(message = "Add Email")
+    @Email(message = "Email not true")
+    @Column
+    private String email;
+
+    public User() {
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
